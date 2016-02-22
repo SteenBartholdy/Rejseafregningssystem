da6e4e4897792b5e2b23449d5007933cef1a7d9e
SteenBartholdy <s145090@student.dtu.dk>
2016-02-22 16:35:02 +0100

    first

---
+++ Rejseafregningssystem/src/dk/dtu/smmac/client/Rejseafregningssystem.java
@@ -0,0 +1,13 @@
+package dk.dtu.smmac.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Rejseafregningssystem implements EntryPoint {
+	
+	public void onModuleLoad() {
+		
+	}
+}
