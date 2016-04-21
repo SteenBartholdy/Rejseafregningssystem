920c7d3795f1492dcaa152bea32d848cc7ba56e7
Roos <s145094@student.dtu.dk>
2016-04-21 09:46:12 +0200

    bank

--- Rejseafregningssystem/src/dk/dtu/smmac/server/dal/BankDAO.java
+++ Rejseafregningssystem/src/dk/dtu/smmac/server/dal/BankDAO.java
@@ -3,6 +3,7 @@ package dk.dtu.smmac.server.dal;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
@@ -33,7 +34,7 @@ public class BankDAO extends RemoteServiceServlet implements BankService {
 			
 			//Laver query, der opretter en konto
 			createBankStmt = connection.prepareStatement("INSERT INTO Konto "
-					+ "( Id, RegNo, KonoNot ) "
+					+ "( Id, RegNo, KontoNo ) "
 					+ "VALUES ( ?, ?, ? );");
 			
 		} catch (SQLException sqlE) {
@@ -43,20 +44,57 @@ public class BankDAO extends RemoteServiceServlet implements BankService {
 
 	@Override
 	public void updateBank(BankDTO bank) throws Exception {
-		// TODO Auto-generated method stub
-		
+		try {
+			updateBankStmt.setInt(1, bank.getRegNo());
+			updateBankStmt.setInt(2, bank.getKontoNo());
+			updateBankStmt.setInt(3, bank.getId());
+			updateBankStmt.executeUpdate();
+		} 
+		catch (SQLException sqlE) {
+			System.out.println(sqlE.getMessage());
+		}
 	}
 
 	@Override
 	public void createBank(BankDTO bank) throws Exception {
-		// TODO Auto-generated method stub
-		
+		try {
+			createBankStmt.setInt(1, bank.getId());
+			createBankStmt.setInt(2, bank.getRegNo());
+			createBankStmt.setInt(3, bank.getKontoNo());
+			createBankStmt.executeUpdate();
+		} 
+		catch (SQLException sqlE) {
+			System.out.println(sqlE.getMessage());
+		} 
 	}
 
 	@Override
 	public BankDTO getBank(int id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		BankDTO bank = null;
+		ResultSet resultSet = null;
+
+		try {
+			getBankStmt.setInt(1, id);
+			resultSet = getBankStmt.executeQuery(); 
+			resultSet.next();
+			bank = new BankDTO(
+					id,
+					resultSet.getInt("RegNo"),
+					resultSet.getInt("KontoNo")
+					);
+		
+		} 
+		catch (SQLException sqlE) {
+			System.out.println(sqlE.getMessage());
+		} 
+		
+		if(bank == null) {
+			bank = new BankDTO(id, 0, 0);
+			
+			createBank(bank);
+		}
+		
+		return bank;
 	}
 	
 }
