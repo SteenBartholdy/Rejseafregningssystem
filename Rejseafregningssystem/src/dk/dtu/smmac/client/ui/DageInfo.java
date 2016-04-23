8591cbb7d484c614338503287fd4d62a1e99b0e4
Anders TT <s140996@student.dtu.dk>
2016-04-23 13:15:25 +0200

    dageinfo

--- Rejseafregningssystem/src/dk/dtu/smmac/client/ui/DageInfo.java
+++ Rejseafregningssystem/src/dk/dtu/smmac/client/ui/DageInfo.java
@@ -23,8 +23,8 @@ public class DageInfo extends Composite
 	private CellTable<RejseDag> table;
 	private VerticalPanel vPanel = new VerticalPanel();
 	private Button btnAnnuller;
-	
-	
+
+
 	private class RejseDag
 	{
 		private String dato;
@@ -35,134 +35,136 @@ public class DageInfo extends Composite
 		//private boolean nattill;
 		//private boolean refunderes;
 		//private boolean afbrudtRejse;
-	
-	@SuppressWarnings("unused")
-	public RejseDag(String dato, String land)
-	{
-		this.dato = dato;
-		this.land = land;
-	}
-	
+
+		@SuppressWarnings("unused")
+		public RejseDag(String dato, String land)
+		{
+			this.dato = dato;
+			this.land = land;
+		}
+
 		public String getRejseDagDato()
 		{
 			return dato;
 		}
-	
+
 		public String getRejseDagLand()
 		{
 			return land;
 		}
-	
+
 		public Boolean getRejseDagMorgenmad()
 		{
 			return morgenmad;
 		}
-	
+
 		public void setRejseDagMorgenmad(boolean morgenmad)
 		{
 			this.morgenmad = morgenmad; 
 		}
-		
+
 		public Boolean getRejseDagFrokost()
 		{
 			return frokost;
 		}
-	
+
 		public void setRejseDagFrokost(boolean frokost)
 		{
 			this.frokost = frokost; 
 		}
 	}
-	
+
+	public void rejsedagetest()
+	{
 	List<RejseDag> rejsedage = new ArrayList<RejseDag>();
-	RejseDag rd = new RejseDag("14-03-20015", "Holland");
-	//rejsedage.add(rd);
+	RejseDag r1 = new RejseDag("14-03-2015", "Holland");
+	RejseDag r2 = new RejseDag("15-03-2015", "Holland");
 	
-	
-//	ArrayList<RejseDag> REJSEDAG = new ArrayList<RejseDag>();
-	
-//	RejseDag dag = new RejseDag(dato, land, morgenmad, frokost, aftensmad, nattill, refunderes, afbrudtRejse);
-//	REJSEDAG.add(new RejseDag(dato, land, morgenmad, frokost, aftensmad, nattill, refunderes, afbrudtRejse));
-	
-/*	@SuppressWarnings("deprecation")
+	rejsedage.add(r1);
+	rejsedage.add(r2);	
+	}
+
+
+	//	ArrayList<RejseDag> REJSEDAG = new ArrayList<RejseDag>();
+
+	//	RejseDag dag = new RejseDag(dato, land, morgenmad, frokost, aftensmad, nattill, refunderes, afbrudtRejse);
+	//	REJSEDAG.add(new RejseDag(dato, land, morgenmad, frokost, aftensmad, nattill, refunderes, afbrudtRejse));
+
+	/*	@SuppressWarnings("deprecation")
 	private static final List<RejseDag> REJSEDAG = Array.asList(
 		new RejseDag("13-05-2015", "Burkinafaso"),
 		new RejseDag("14-05-2015", "Burkinafaso"),
 		new RejseDag("15-05-2015", "Burkinafaso"),
 		new RejseDag("16-05-2015", "Burkinafaso"));
-*/
-	
+	 */
+
 	public DageInfo()
 	{
 		initWidget(this.vPanel);
-		
+
 		table = new CellTable<RejseDag>();
 		table.setPageSize(4);
+
+		rejsedagetest();
 		
 		btnAnnuller = new Button("Annuller");
-		
+
 		TextColumn<RejseDag> datoColumn = new TextColumn<RejseDag>()
 		{
 			@Override
-		      public String getValue(RejseDag object) 
-				{
-		        return object.getRejseDagDato();
-				}
+			public String getValue(RejseDag object) 
+			{
+				return object.getRejseDagDato();
+			}
 		};
 		table.addColumn(datoColumn, "Dato:");
-	
+
 		TextColumn<RejseDag> landColumn = new TextColumn<RejseDag>()
 		{
 			@Override
-		      public String getValue(RejseDag object) 
-				{
-		        return object.getRejseDagLand();
-				}
+			public String getValue(RejseDag object) 
+			{
+				return object.getRejseDagLand();
+			}
 		};
 		table.addColumn(landColumn, "Land:");
-		
+
 		Column<RejseDag, Boolean> morgenmadColumn = new Column<RejseDag, Boolean>(new CheckboxCell()) { 
-		      @Override 
-		      public Boolean getValue(RejseDag object) { 
-		        return object.getRejseDagMorgenmad(); 
-		      } 
-		    }; 
+			@Override 
+			public Boolean getValue(RejseDag object) { 
+				return object.getRejseDagMorgenmad(); 
+			} 
+		}; 
 
-		    morgenmadColumn.setFieldUpdater(new FieldUpdater<RejseDag, Boolean>() { 
-		      public void update(int index, RejseDag object, Boolean value) {
-		        object.setRejseDagMorgenmad(value); 
-		      } 
-		    }); 
-		    
-		    table.addColumn(morgenmadColumn, "Morgenmad:");
-		    
-		    Column<RejseDag, Boolean> frokostColumn = new Column<RejseDag, Boolean>(new CheckboxCell()) { 
-			      @Override 
-			      public Boolean getValue(RejseDag object) { 
-			        return object.getRejseDagFrokost(); 
-			      } 
-			    }; 
+		morgenmadColumn.setFieldUpdater(new FieldUpdater<RejseDag, Boolean>() { 
+			public void update(int index, RejseDag object, Boolean value) {
+				object.setRejseDagMorgenmad(value); 
+			} 
+		}); 
 
-			    frokostColumn.setFieldUpdater(new FieldUpdater<RejseDag, Boolean>() { 
-			      public void update(int index, RejseDag object, Boolean value) {
-			        object.setRejseDagFrokost(value); 
-			      } 
-			    }); 
-			    
-			    table.addColumn(frokostColumn, "Frokost:");
-		
-		
-		
-		
-		
-		
-		
-		
+		table.addColumn(morgenmadColumn, "Morgenmad:");
+
+		Column<RejseDag, Boolean> frokostColumn = new Column<RejseDag, Boolean>(new CheckboxCell()) { 
+			@Override 
+			public Boolean getValue(RejseDag object) { 
+				return object.getRejseDagFrokost(); 
+			} 
+		}; 
+
+		frokostColumn.setFieldUpdater(new FieldUpdater<RejseDag, Boolean>() { 
+			public void update(int index, RejseDag object, Boolean value) {
+				object.setRejseDagFrokost(value); 
+			} 
+		}); 
+
+		table.addColumn(frokostColumn, "Frokost:");
+
 		vPanel.add(table);
 		vPanel.add(btnAnnuller);
-		
+
 		vPanel.setStyleName("margin");
 	
+
 	}
 
 	public Button getbtnAnnullerDageInfo()
---
+++ Rejseafregningssystem/src/dk/dtu/smmac/shared/DageInfoDTO.java
@@ -0,0 +1,58 @@
+package dk.dtu.smmac.shared;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class DageInfoDTO implements Serializable
+{
+	private static final long serialVersionUID = 6L;
+
+	private int RejseID;
+	private int Nummer;
+	private String Land;
+	private String By;
+	private Date DatoFra;
+	private Date DatoTil;
+	
+	public DageInfoDTO() {}
+	
+	public DageInfoDTO(int RejseID, int Nummer, String Land, String By, Date DatoFra, Date DatoTil)
+	{
+		this.RejseID = RejseID;
+		this.Nummer = Nummer;
+		this.Land = Land;
+		this.By = By;
+		this.DatoFra = DatoFra;
+		this.DatoTil = DatoTil;
+	}
+
+	public int getRejseID()
+	{
+		return this.RejseID;
+	}
+
+	public int getNummer()
+	{
+		return this.Nummer;
+	}
+
+	public String getLand()
+	{
+		return this.Land;
+	}
+
+	public String getBy()
+	{
+		return this.By;
+	}
+	
+	public Date getDatoFra()
+	{
+		return this.DatoFra;
+	}
+
+	public Date getDatoTil()
+	{
+		return this.DatoTil;
+	}
+}
