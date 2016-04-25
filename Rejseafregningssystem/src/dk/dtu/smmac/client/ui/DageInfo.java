d0a81b80f796015b05a0d254472929a4b4be110a
Anders TT <s140996@student.dtu.dk>
2016-04-25 14:47:31 +0200

    dageinfo

--- Rejseafregningssystem/src/dk/dtu/smmac/client/ui/DageInfo.java
+++ Rejseafregningssystem/src/dk/dtu/smmac/client/ui/DageInfo.java
@@ -1,97 +1,165 @@
 package dk.dtu.smmac.client.ui;
 
+import com.google.gwt.cell.client.CheckboxCell;
+import com.google.gwt.cell.client.FieldUpdater;
 import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.Column;
+import com.google.gwt.user.cellview.client.HasKeyboardSelectionPolicy.KeyboardSelectionPolicy;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.view.client.ProvidesKey;
 
-import dk.dtu.smmac.shared.RejseDTO;
+import dk.dtu.smmac.shared.DageInfoDTO;
 
 
 public class DageInfo extends Composite 
 {
-	private CellTable<RejseDTO> table;
+	private CellTable<DageInfoDTO> table;
 	private VerticalPanel vPanel = new VerticalPanel();
 	private Button btnAnnuller;
 	DateTimeFormat dtFmt = DateTimeFormat.getFormat("dd/MM yyyy");
 
-	 private static final ProvidesKey<RejseDTO> KEY_PROVIDER = new ProvidesKey<RejseDTO>() {
+	 private static final ProvidesKey<DageInfoDTO> KEY_PROVIDER = new ProvidesKey<DageInfoDTO>() {
 		    @Override
-		    public Object getKey(RejseDTO dag) {
-		      return dag.getRejseID();
+		    public Object getKey(DageInfoDTO dag) {
+		      return dag.getDagID();
 		    }
 		  };
 	
 	public DageInfo()//RejseDAO did)
 	{
-//		table = new CellTable<RejseDTO>(KEY_PROVIDER);
-//		table.setKeyboardSelectionPolicy(KeyboardSelectionPolicy.ENABLED);
-//		table.setPageSize(4);
-//		
-//		initWidget(this.vPanel);
-//		
-//		btnAnnuller = new Button("Annuller");
-//
-//		TextColumn<RejseDag> datoColumn = new TextColumn<RejseDag>()
-//		{
-//			@Override
-//			public String getValue(RejseDag object) 
-//			{
-//				return object.getRejseDagDato();
-//			}
-//		};
-//		table.addColumn(datoColumn, "Dato:");
-//
-//		TextColumn<RejseDag> landColumn = new TextColumn<RejseDag>()
-//		{
-//			@Override
-//			public String getValue(RejseDag object) 
-//			{
-//				return object.getRejseDagLand();
-//			}
-//		};
-//		table.addColumn(landColumn, "Land:");
-//
-//		Column<RejseDag, Boolean> morgenmadColumn = new Column<RejseDag, Boolean>(new CheckboxCell()) { 
-//			@Override 
-//			public Boolean getValue(RejseDag object) { 
-//				return object.getRejseDagMorgenmad(); 
-//			} 
-//		}; 
-//
-//		morgenmadColumn.setFieldUpdater(new FieldUpdater<RejseDag, Boolean>() { 
-//			@Override
-//			public void update(int index, RejseDag object, Boolean value) {
-//				object.setRejseDagMorgenmad(value); 
-//			} 
-//		}); 
-//
-//		table.addColumn(morgenmadColumn, "Morgenmad:");
-//
-//		Column<RejseDag, Boolean> frokostColumn = new Column<RejseDag, Boolean>(new CheckboxCell()) { 
-//			@Override 
-//			public Boolean getValue(RejseDag object) { 
-//				return object.getRejseDagFrokost(); 
-//			} 
-//		}; 
-//
-//		frokostColumn.setFieldUpdater(new FieldUpdater<RejseDag, Boolean>() { 
-//			@Override
-//			public void update(int index, RejseDag object, Boolean value) {
-//				object.setRejseDagFrokost(value); 
-//			} 
-//		}); 
-//
-//		table.addColumn(frokostColumn, "Frokost:");
-//
-//		vPanel.add(table);
-//		vPanel.add(btnAnnuller);
+		table = new CellTable<DageInfoDTO>(KEY_PROVIDER);
+		table.setKeyboardSelectionPolicy(KeyboardSelectionPolicy.ENABLED);
+		table.setPageSize(4);
+		
+		initWidget(this.vPanel);
+		
+		btnAnnuller = new Button("Annuller");
 
+		// **** Add a checkbox input column that shows "Morgenmad" ****
+		
+	    Column<DageInfoDTO, Boolean> morgenmadColumn = new Column<DageInfoDTO, Boolean>(new CheckboxCell()) { 
+	      @Override 
+	      public Boolean getValue(DageInfoDTO object) { 
+	        return object.getMorgenmad(); 
+	      } 
+	    }; 
+
+	    morgenmadColumn.setFieldUpdater(new FieldUpdater<DageInfoDTO, Boolean>() { 
+	      public void update(int index, DageInfoDTO object, Boolean value) {
+	        object.setMorgenmad(value); 
+	      } 
+	    }); 
+	    
+	    table.addColumn(morgenmadColumn, "Morgenmad:");
+		
+	    // **** Add a checkbox input column that shows "Frokost" ****
+	    
+	    Column<DageInfoDTO, Boolean> frokostColumn = new Column<DageInfoDTO, Boolean>(new CheckboxCell()) { 
+	      @Override 
+	      public Boolean getValue(DageInfoDTO object) { 
+	        return object.getFrokost(); 
+	      } 
+	    }; 
+
+	    frokostColumn.setFieldUpdater(new FieldUpdater<DageInfoDTO, Boolean>() { 
+	      public void update(int index, DageInfoDTO object, Boolean value) {
+	        object.setFrokost(value); 
+	      } 
+	    }); 
+	    
+	    table.addColumn(frokostColumn, "Frokost:");
+	    
+	    // **** Add a checkbox input column that shows "Aftensmad" ****
+	    
+	    Column<DageInfoDTO, Boolean> aftensmadColumn = new Column<DageInfoDTO, Boolean>(new CheckboxCell()) { 
+	      @Override 
+	      public Boolean getValue(DageInfoDTO object) { 
+	        return object.getAftensmad(); 
+	      } 
+	    }; 
+
+	    aftensmadColumn.setFieldUpdater(new FieldUpdater<DageInfoDTO, Boolean>() { 
+	      public void update(int index, DageInfoDTO object, Boolean value) {
+	        object.setAftensmad(value); 
+	      } 
+	    }); 
+	    
+	    table.addColumn(aftensmadColumn, "Aftensmad:");
+	    
+	    // **** Add a checkbox input column that shows "Nattillæg" ****
+	    
+	    Column<DageInfoDTO, Boolean> nattillColumn = new Column<DageInfoDTO, Boolean>(new CheckboxCell()) { 
+	      @Override 
+	      public Boolean getValue(DageInfoDTO object) { 
+	        return object.getNattill(); 
+	      } 
+	    }; 
+
+	    nattillColumn.setFieldUpdater(new FieldUpdater<DageInfoDTO, Boolean>() { 
+	      public void update(int index, DageInfoDTO object, Boolean value) {
+	        object.setNattill(value); 
+	      } 
+	    }); 
+	    
+	    table.addColumn(nattillColumn, "Nattillæg:");
+
+	    // **** Add a checkbox input column that shows "Rejse Afbrudt" ****
+	    
+	    Column<DageInfoDTO, Boolean> rejseafbrudtColumn = new Column<DageInfoDTO, Boolean>(new CheckboxCell()) { 
+	      @Override 
+	      public Boolean getValue(DageInfoDTO object) { 
+	        return object.getRejseAfbrudt(); 
+	      } 
+	    }; 
+
+	    rejseafbrudtColumn.setFieldUpdater(new FieldUpdater<DageInfoDTO, Boolean>() { 
+	      public void update(int index, DageInfoDTO object, Boolean value) {
+	        object.setRejseAfbrudt(value); 
+	      } 
+	    }); 
+	    
+	    table.addColumn(rejseafbrudtColumn, "Rejse Afbrudt:");
+	    
+	    // **** Add a checkbox input column that shows "Udokumenteret nattillæg" ****
+	    
+	    Column<DageInfoDTO, Boolean> udoknatColumn = new Column<DageInfoDTO, Boolean>(new CheckboxCell()) { 
+	      @Override 
+	      public Boolean getValue(DageInfoDTO object) { 
+	        return object.getUdokNat(); 
+	      } 
+	    }; 
+
+	    udoknatColumn.setFieldUpdater(new FieldUpdater<DageInfoDTO, Boolean>() { 
+	      public void update(int index, DageInfoDTO object, Boolean value) {
+	        object.setUdokNat(value); 
+	      } 
+	    }); 
+	    
+	    table.addColumn(udoknatColumn, "Udokumenteret Nattillæg:");
+	    
+	    // **** Add a checkbox input column that shows "Refunderes" ****
+	    
+	    Column<DageInfoDTO, Boolean> refunderesColumn = new Column<DageInfoDTO, Boolean>(new CheckboxCell()) { 
+	      @Override 
+	      public Boolean getValue(DageInfoDTO object) { 
+	        return object.getRefunderes(); 
+	      } 
+	    }; 
+
+	    refunderesColumn.setFieldUpdater(new FieldUpdater<DageInfoDTO, Boolean>() { 
+	      public void update(int index, DageInfoDTO object, Boolean value) {
+	        object.setRefunderes(value); 
+	      } 
+	    }); 
+	    
+	    table.addColumn(refunderesColumn, "Refunderes:");
+	    
+		vPanel.add(table);
+		vPanel.add(btnAnnuller);
 		vPanel.setStyleName("margin");
-	
-
 	}
 
 	public Button getbtnAnnullerDageInfo()
--- Rejseafregningssystem/src/dk/dtu/smmac/shared/DageInfoDTO.java
+++ Rejseafregningssystem/src/dk/dtu/smmac/shared/DageInfoDTO.java
@@ -2,6 +2,7 @@ package dk.dtu.smmac.shared;
 
 import java.io.Serializable;
 
+
 public class DageInfoDTO implements Serializable {
 
 	private static final long serialVersionUID = 7L;
@@ -46,9 +47,19 @@ public class DageInfoDTO implements Serializable {
 		return this.morgenmad;
 	}
 	
+	public void setMorgenmad(boolean morgenmad)
+	{
+		this.morgenmad = morgenmad;
+	}
+	
 	public boolean getFrokost()
 	{
 		return this.frokost;
+	}
+	
+	public void setFrokost(boolean frokost)
+	{
+		this.frokost = frokost;
 	}
 	
 	public boolean getAftensmad()
@@ -56,23 +67,48 @@ public class DageInfoDTO implements Serializable {
 		return this.aftensmad;
 	}
 
+	public void setAftensmad(boolean aftensmad)
+	{
+		this.aftensmad = aftensmad;
+	}
+	
 	public boolean getNattill()
 	{
 		return this.nattill;
 	}
 
+	public void setNattill(boolean nattill)
+	{
+		this.nattill = nattill;
+	}
+	
 	public boolean getRejseAfbrudt()
 	{
 		return this.rejseAfbrudt;
 	}
 
+	public void setRejseAfbrudt(boolean rejseafbrudt)
+	{
+		this.rejseAfbrudt = rejseafbrudt;
+	}
+	
 	public boolean getUdokNat()
 	{
 		return this.udokNat;
 	}
 
+	public void setUdokNat(boolean udoknat)
+	{
+		this.udokNat = udoknat;
+	}
+	
 	public boolean getRefunderes()
 	{
 		return this.refunderes;
+	}
+
+	public void setRefunderes(boolean refunderes)
+	{
+		this.refunderes = refunderes;
 	}
 }
