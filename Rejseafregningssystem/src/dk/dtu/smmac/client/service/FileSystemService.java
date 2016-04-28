af57aca52d3717cd1c0706ed5c7532fcc976b0b9
ChristofferSvendsen <s145089@student.dtu.dk>
2016-04-28 15:37:46 +0200

    FileSystem

---
+++ Rejseafregningssystem/src/dk/dtu/smmac/client/service/FileSystemService.java
@@ -0,0 +1,11 @@
+package dk.dtu.smmac.client.service;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+@RemoteServiceRelativePath("filesystemservice")
+public interface FileSystemService extends RemoteService{
+	
+	public void uploadFile(String bucketName, String keyName, String filePath) throws Exception;
+
+}
---
+++ Rejseafregningssystem/src/dk/dtu/smmac/client/service/FileSystemServiceAsync.java
@@ -0,0 +1,9 @@
+package dk.dtu.smmac.client.service;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public interface FileSystemServiceAsync {
+	
+	void uploadFile(String bucketName, String keyName, String filePath, AsyncCallback<Void> callback);
+
+}
