59cf4cf0e29d52110777b9894cde06ee8d5a2a3b
Roos <s145094@student.dtu.dk>
2016-04-29 09:51:20 +0200

    rejseafregningerDTO

---
+++ Rejseafregningssystem/src/dk/dtu/smmac/shared/RejseafregningerDTO.java
@@ -0,0 +1,76 @@
+package dk.dtu.smmac.shared;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class RejseafregningerDTO implements Serializable {
+
+	private static final long serialVersionUID = 11L;
+
+	private int nr, startTid, slutTid;
+	private String land;
+	private Date datoFra, datoTil;
+	
+	public RejseafregningerDTO() {
+		super();
+	}
+	
+	public RejseafregningerDTO(int nummer, int startTid, int sluttid, String land, Date datoFra, Date datoTil) {
+		super();
+		this.nr = nummer;
+		this.startTid = startTid;
+		this.slutTid = sluttid;
+		this.land = land;
+		this.datoFra = datoFra;
+		this.datoTil = datoTil;
+	}
+	
+	public int getNr() {
+		return nr;
+	}
+
+	public void setNr(int nr) {
+		this.nr = nr;
+	}
+
+	public int getStartTid() {
+		return startTid;
+	}
+
+	public void setStartTid(int startTid) {
+		this.startTid = startTid;
+	}
+
+	public int getSlutTid() {
+		return slutTid;
+	}
+
+	public void setSlutTid(int slutTid) {
+		this.slutTid = slutTid;
+	}
+
+	public String getLand() {
+		return land;
+	}
+
+	public void setLand(String land) {
+		this.land = land;
+	}
+
+	public Date getDatoFra() {
+		return datoFra;
+	}
+
+	public void setDatoFra(Date datoFra) {
+		this.datoFra = datoFra;
+	}
+
+	public Date getDatoTil() {
+		return datoTil;
+	}
+
+	public void setDatoTil(Date datoTil) {
+		this.datoTil = datoTil;
+	}
+	
+}
