ab1a8423323e0a7295c5aa21ddbbda1a1b2e7546
SteenBartholdy <s145090@student.dtu.dk>
2016-05-02 16:28:27 +0200

    skldjflkdsjf

--- Rejseafregningssystem/src/dk/dtu/smmac/client/service/LandeServiceAsync.java
+++ Rejseafregningssystem/src/dk/dtu/smmac/client/service/LandeServiceAsync.java
@@ -1,5 +1,7 @@
 package dk.dtu.smmac.client.service;
 
+import java.util.List;
+
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public interface LandeServiceAsync {
@@ -8,5 +10,6 @@ public interface LandeServiceAsync {
 	void getTakst(int takst, AsyncCallback<Integer> callback);
 	void setLand(String land, AsyncCallback<Void> callback);
 	void setTakst(int takst, AsyncCallback<Void> callback);
+	void getAllLande(List<String> land, AsyncCallback<List<String>> callback);
 	
 }
--- Rejseafregningssystem/src/dk/dtu/smmac/server/dal/LandDAO.java
+++ Rejseafregningssystem/src/dk/dtu/smmac/server/dal/LandDAO.java
@@ -3,11 +3,17 @@ package dk.dtu.smmac.server.dal;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 import dk.dtu.smmac.client.service.LandeService;
+import dk.dtu.smmac.shared.AnsatDTO;
+import dk.dtu.smmac.shared.DageInfoDTO;
 
 public class LandDAO extends RemoteServiceServlet implements LandeService {
 
@@ -16,6 +22,8 @@ public class LandDAO extends RemoteServiceServlet implements LandeService {
 	private PreparedStatement getLandStmt = null;
 	private PreparedStatement getTakstStmt = null;
 	private PreparedStatement setLandStmt = null;
+	private PreparedStatement setTakstStmt = null;
+	private PreparedStatement getAllLandeStmt = null;
 	
 	public LandDAO() throws Exception
 	{
@@ -23,9 +31,6 @@ public class LandDAO extends RemoteServiceServlet implements LandeService {
 			Class.forName("com.mysql.jdbc.Driver");
 			connection = DriverManager.getConnection(DAO.URL, DAO.USERNAME, DAO.PASSWORD);
 			
-			
-
-
 		} catch (SQLException sqlE) {
 			System.out.println(sqlE.getMessage());
 		}
@@ -53,6 +58,32 @@ public class LandDAO extends RemoteServiceServlet implements LandeService {
 	@Override
 	public void setTakst(int takst) throws Exception {
 		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<String> getAllLande() throws Exception {
+		getLandStmt = connection.prepareStatement("SELECT * From Lande;");
+		
+		List<String> land = null;
+		ResultSet resultSet = null;
+
+
+		try {
+		
+			resultSet = getAllLandeStmt.executeQuery(); 
+			land = new ArrayList<String>();
+			
+			while(resultSet.next())
+			{
+				land.add(resultSet.getString("Land"));
+			}
+
+		} catch (SQLException sqlE) {
+			System.out.println(sqlE.getMessage());
+		} 
+
+		return land;
 		
 	}
 
