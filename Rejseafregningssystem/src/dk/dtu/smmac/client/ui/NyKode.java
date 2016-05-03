0b91e222d9bfc1c08e7e984617ede6e289e315e5
Anders TT <s140996@student.dtu.dk>
2016-05-03 12:31:57 +0200

    nykode opdatering

--- Rejseafregningssystem/src/dk/dtu/smmac/client/ui/NyKode.java
+++ Rejseafregningssystem/src/dk/dtu/smmac/client/ui/NyKode.java
@@ -2,18 +2,23 @@ package dk.dtu.smmac.client.ui;
 
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class NyKode extends Composite 
 {
 	private VerticalPanel vPanel = new VerticalPanel();
+	private HorizontalPanel hPanel = new HorizontalPanel();
 	private Label lNyKode, lNyKodeVeri, lGammelKode, lBrugernavn;
 	private Button btnTilbage, btnUdfoer;
-	private TextBox tbNyKode, tbNyKodeVeri, tbGammelKode, tbBrugernavn;	
-	private String lwidth = "100px";
+	private TextBox tbBrugernavn;
+	private PasswordTextBox tbNyKode, tbNyKodeVeri, tbGammelKode;
+	private String lwidth = "150px";
 	private String height = "20px";
+	private String bwidth = "50px";
 	
 	public NyKode()
 	{
@@ -29,22 +34,24 @@ public class NyKode extends Composite
 		lBrugernavn.setStyleName("boldTextPlusMargin");
 		
 		btnTilbage = new Button("Tilbage");
+		btnTilbage.setWidth(bwidth);
 		btnUdfoer = new Button("Udfør");
-		btnUdfoer.setStyleName("marginButtom");
+		btnUdfoer.setWidth(bwidth);
+		btnUdfoer.setStyleName("marginRight");
 		
 		tbBrugernavn = new TextBox();
 		tbBrugernavn.setWidth(lwidth);
 		tbBrugernavn.setHeight(height);
 		tbBrugernavn.setStyleName("marginButtom");
-		tbNyKode = new TextBox();
+		tbNyKode = new PasswordTextBox();
 		tbNyKode.setWidth(lwidth);
 		tbNyKode.setHeight(height);
 		tbNyKode.setStyleName("marginButtom");
-		tbNyKodeVeri = new TextBox();
+		tbNyKodeVeri = new PasswordTextBox();
 		tbNyKodeVeri.setWidth(lwidth);
 		tbNyKodeVeri.setHeight(height);
 		tbNyKodeVeri.setStyleName("marginButtom");
-		tbGammelKode = new TextBox();
+		tbGammelKode = new PasswordTextBox();
 		tbGammelKode.setWidth(lwidth);
 		tbGammelKode.setHeight(height);
 		tbGammelKode.setStyleName("marginButtom");
@@ -57,8 +64,9 @@ public class NyKode extends Composite
 		vPanel.add(tbNyKode);
 		vPanel.add(lNyKodeVeri);
 		vPanel.add(tbNyKodeVeri);
-		vPanel.add(btnUdfoer);
-		vPanel.add(btnTilbage);	
+		vPanel.add(hPanel);
+		hPanel.add(btnTilbage);	
+		hPanel.add(btnUdfoer);
 		
 		vPanel.setStyleName("margin");
 	}
