c5723e8be2581d10d4aa5dc0aa90ed76a624ffb0
Anders TT <s140996@student.dtu.dk>
2016-05-03 13:32:33 +0200

    ny kode skcjndskjcnkjsdnj

--- Rejseafregningssystem/src/dk/dtu/smmac/client/ui/NyKode.java
+++ Rejseafregningssystem/src/dk/dtu/smmac/client/ui/NyKode.java
@@ -11,10 +11,8 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 public class NyKode extends Composite 
 {
 	private VerticalPanel vPanel = new VerticalPanel();
-	private HorizontalPanel hPanel1 = new HorizontalPanel();
-	private HorizontalPanel hPanel2 = new HorizontalPanel();
 	private Label lNyKode, lNyKodeVeri, lGammelKode, lBrugernavn, lBrugernavnHentet;
-	private Button btnTilbage, btnUdfoer, btnUsynlig;
+	private Button btnTilbage, btnUdfoer;
 	private PasswordTextBox tbNyKode, tbNyKodeVeri, tbGammelKode;
 	private String lwidth = "150px";
 	private String height = "20px";
@@ -31,16 +29,13 @@ public class NyKode extends Composite
 		lNyKodeVeri.setStyleName("boldTextPlusMargin");
 		lBrugernavn = new Label("Brugernavn:  ");
 		lBrugernavn.setStyleName("boldTextPlusMargin");
-		lBrugernavnHentet = new Label();
-		lBrugernavnHentet.setStyleName("marginButtom");
+		lBrugernavnHentet = new Label(" ");
+		lBrugernavnHentet.setStyleName("boldTextPlusMargin");
 		
 		btnTilbage = new Button("Tilbage");
 		btnTilbage.setStyleName("buttonNyKode");
 		btnUdfoer = new Button("Udfør");
 		btnUdfoer.setStyleName("buttonNyKode");
-		btnUsynlig = new Button();
-		btnUsynlig.setStyleName("buttonNyKode");
-		btnUsynlig.setVisible(false);
 				
 		tbNyKode = new PasswordTextBox();
 		tbNyKode.setWidth(lwidth);
@@ -55,19 +50,18 @@ public class NyKode extends Composite
 		tbGammelKode.setHeight(height);
 		tbGammelKode.setStyleName("marginButtom");
 		
-		vPanel.add(hPanel1);
-		hPanel1.add(lBrugernavn);
-		hPanel1.add(lBrugernavnHentet);
+		
+		vPanel.add(lBrugernavn);
+		vPanel.add(lBrugernavnHentet);
 		vPanel.add(lGammelKode);
 		vPanel.add(tbGammelKode);
 		vPanel.add(lNyKode);
 		vPanel.add(tbNyKode);
 		vPanel.add(lNyKodeVeri);
 		vPanel.add(tbNyKodeVeri);
-		vPanel.add(hPanel2);
-		hPanel2.add(btnTilbage);	
-		hPanel2.add(btnUdfoer);
-		hPanel2.add(btnUsynlig);
+		vPanel.add(btnUdfoer);
+		vPanel.add(btnTilbage);	
+	
 		
 		vPanel.setStyleName("margin");
 	}
