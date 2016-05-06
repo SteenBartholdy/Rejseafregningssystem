3408ed30632510f31aecc0c4c9d22508f52a7c74
ChristofferSvendsen <s145089@student.dtu.dk>
2016-05-06 09:53:32 +0200

    ...

--- Rejseafregningssystem/src/dk/dtu/smmac/shared/FileSystemDTO.java
+++ Rejseafregningssystem/src/dk/dtu/smmac/shared/FileSystemDTO.java
@@ -15,10 +15,42 @@ public class FileSystemDTO implements Serializable {
     
     public FileSystemDTO(String bucketName, String keyName, String filePath)
     {
+    	super();
     	this.existingBucketName = bucketName;
     	this.keyName = keyName;
     	this.filePath = filePath;
     }
+
+    public String getBucketName()
+    {
+    	return this.existingBucketName;
+    }
+    
+    public String getKeyName()
+    {
+    	return this.keyName;
+    }
+    
+    public String getFilePath()
+    {
+    	return this.filePath;
+    }
+    
+    public void setBucketName(String bucketName)
+    {
+    	this.existingBucketName = bucketName;
+    }
+    
+    public void setKeyName(String keyName)
+    {
+    	this.keyName = keyName;
+    }
+    
+    public void setFilePath(String filePath)
+    {
+    	this.filePath = filePath;
+    }
+
     
     
 
